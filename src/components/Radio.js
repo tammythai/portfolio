fe60c9bac4eb0333245f3602bd31af61a69a119d
Tammy Thai <tammythai92@gmail.com>
2021-03-14 14:18:32 -0700

    Modified server to use spotify api node library

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Radio() {
+  const [tracks, setTracks] = useState([]);
+  // use useEffect to setTracks to the filtered list from
+  // fetching /api/radio
+  // you can extract out the function to an api functions file
+  // have it so that useEffect is only called when tracks changes
+
   return <div>This be the radio yeet</div>;
 }
 
